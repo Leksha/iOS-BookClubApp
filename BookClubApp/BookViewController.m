3105e3e499f529a2c82e98e712e3db2ddebc9595
Leksha Ramdenee <leksha.ramdenee@tulip.io>
2016-12-09 10:55:12 -0500

    Added nextMeeting field

--- BookClubApp/BookViewController.m
+++ BookClubApp/BookViewController.m
@@ -14,8 +14,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *bookTitleText;
 @property (weak, nonatomic) IBOutlet UITextField *bookAuthorText;
 @property (weak, nonatomic) IBOutlet UITextField *bookChaptersText;
+@property (weak, nonatomic) IBOutlet UITextField *bookNextMeeting;
 
-
+@property (nonatomic) BOOL firstLaunch;
 @end
 
 @implementation BookViewController
@@ -24,10 +25,20 @@
     self = [super init];
     if (self) {
         _currentBook = [Book sharedStore];
-        self.tabBarItem.title = @"Book Selected";
+        self.tabBarItem.title = @"Book Details";
         self.tabBarItem.image = [UIImage imageNamed:@"book-bookmark"];
+        _firstLaunch = true;
     }
     return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    if (!_firstLaunch){
+        _bookTitleText.text = _currentBook.bookTitle;
+        _bookAuthorText.text = _currentBook.bookAuthor;
+        _bookChaptersText.text = [NSString stringWithFormat:@"%i", _currentBook.bookChapters];
+        _bookNextMeeting.text = _currentBook.nextMeeting;
+    }
 }
 
 - (void)viewDidLoad {
@@ -45,6 +56,8 @@
     book.bookTitle = self.bookTitleText.text;
     book.bookAuthor = self.bookAuthorText.text;
     book.bookChapters = [self.bookChaptersText.text intValue];
+    book.nextMeeting = self.bookNextMeeting.text;
+    _currentBook = book;
     
     CATransition* transition = [CATransition animation];
     transition.duration = 1.0;
